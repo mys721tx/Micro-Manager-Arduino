1edbe125f6660b80c1267e272e795ab54b5a4525
Yishen Miao <mys721tx@gmail.com>
2021-08-06 23:15:54 -0700

    Reading parameters from serial
    Reads parameters from serial.

--- ExternalTimer/ExternalTimer.ino
+++ ExternalTimer/ExternalTimer.ino
@@ -1,26 +1,26 @@
 #include <cppQueue.h>
 
-void (*resetFunc)(void) = 0;
+void (*reset)(void) = 0;
 
-const byte pin_intr = 2;
-const byte mask_low = 0b00111111;
+const uint8_t pin_intr = 2;
+const uint8_t mask_low = 0b00111111;
 
 volatile bool enabled = false;
 
-typedef struct {
-	unsigned long time;
-	byte pattern;
-} TimePoint;
+enum State { STANDBY, CONFIG, SETUP, RUN };
 
-TimePoint pts[3] = { { 5000000, 0b00000000 },
-		     { 5000000, 0b00111111 },
-		     { 5000000, 0b00000000 } };
+typedef struct {
+	uint32_t time;
+	uint8_t pattern;
+} TimePoint;
 
 cppQueue q(sizeof(TimePoint));
 
 TimePoint *pt_cur;
 
-unsigned long t_prev = 0;
+uint32_t t_prev;
+
+State state = STANDBY;
 
 void setup()
 {
@@ -28,31 +28,66 @@ void setup()
 	pinMode(pin_intr, INPUT);
 	DDRB = 0b00111111;
 	PORTB = 0b00000000;
-
-	attachInterrupt(digitalPinToInterrupt(pin_intr), enable, RISING);
-
-	for (int i = 0; i < 3; i++) {
-		TimePoint pt = pts[i];
-		q.push(&pt);
-	}
-
-	q.pop(pt_cur);
 }
 
 void loop()
 {
-	unsigned long t_cur = micros();
+	switch (state) {
+	case STANDBY: {
+		Serial.println("Standby");
 
-	if (enabled) {
-		PORTB = pt_cur->pattern & mask_low;
-		if (t_cur - t_prev >= pt_cur->time) {
-			t_prev = t_cur;
-			if (!q.pop(pt_cur)) {
-				resetFunc();
+		state = SETUP;
+		break;
+	}
+	case CONFIG: {
+		char buf[sizeof(TimePoint)];
+
+		if (Serial.available() > 0) {
+			for (int i = 0; i < sizeof(TimePoint); i++) {
+				buf[i] = Serial.read();
 			}
 		}
-	} else {
-		t_prev = t_cur;
+
+		uint32_t time = ((buf[0] & 0xFF) << 24) |
+				     ((buf[1] & 0xFF) << 16) |
+				     ((buf[2] & 0xFF) << 8) | (buf[3] & 0xFF);
+
+		uint8_t pattern = buf[4];
+
+		TimePoint pt = TimePoint{ time, pattern };
+
+		q.push(&pt);
+
+		if (q.isFull()) {
+			state = SETUP;
+		}
+		break;
+	}
+	case SETUP: {
+		attachInterrupt(digitalPinToInterrupt(pin_intr), enable,
+				RISING);
+
+		if (!q.pop(pt_cur)) {
+			reset();
+		}
+		state = RUN;
+	}
+	case RUN: {
+		uint32_t t_cur = micros();
+
+		if (enabled) {
+			PORTB = pt_cur->pattern & mask_low;
+			if (t_cur - t_prev >= pt_cur->time) {
+				t_prev = t_cur;
+				if (!q.pop(pt_cur)) {
+					reset();
+				}
+			}
+		} else {
+			t_prev = t_cur;
+		}
+		break;
+	}
 	}
 }
 
